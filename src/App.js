d1f288c50359cfedecf020bc67a67663e6f570e3
Isai Rodriguez <macover1507@hotmail.com>
2023-02-12 18:29:29 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+import htmlDocx from 'html-docx-js/dist/html-docx';
+import { saveAs } from "file-saver";
+import htmlParser from 'html-react-parser'
+import { renderToString } from 'react-dom/server'
+import { useState } from 'react';
+
 function App() {
+
+  const [fileName, setFileName] = useState('Upload a HTML file.');
+  const [htmlObject, setHtmlObject] = useState();
+
+  let fileReader;
+
+  const handleReader = async (e) => {
+    const file = e.target.files[0]
+    setFileName(file.name)
+    fileReader = new FileReader();
+    fileReader.onloadend = (e) => {
+      setHtmlObject(htmlParser(fileReader.result))
+    }
+    fileReader.readAsText(file)
+  }
+
+  const convertHtmlToDocx = () => {
+
+    const bodyContent = renderToString(
+      htmlObject.props.children.find(children => children.type === 'body')
+    );
+
+    const content = `
+          <html lang="en">
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            ${bodyContent}
+          </html>`;
+    
+    var data = htmlDocx.asBlob(content);
+    const newFileName = fileName.split('.html')[0];
+    saveAs(data, `${newFileName}.docx`);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='App-content'>
+          <p>
+            HTML to <code>DOCX</code>
+          </p>
+          <div className='content-input'>
+            <input
+              type='text'
+              placeholder={fileName}
+              disabled />
+            <input
+              accept='.html'
+              onChange={(e) => handleReader(e)}
+              type='file' />
+          </div>
+          <button
+            onClick={convertHtmlToDocx}
+          >Download .docx file</button>
+        </div>
       </header>
     </div>
   );
